5de96c67e017e2b60c32834fd2d342281ccf35bc
jb_labs <79516500+jb-labs-456@users.noreply.github.com>
2024-10-05 21:33:00 +0100

    Ground Sensor Code Fixed
    Added delays between different sensor calls. Final working version is v2.2

---
+++ GroundSensorCode_v2.1/Setup.ino
@@ -0,0 +1,33 @@
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  dht.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+    server.on("/moisture", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readMoisture().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
+
+  // Start server
+  server.begin();
+}
